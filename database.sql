2894cfc9963eb061660153e6bd0ecc68a90ae55d
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-06-21 22:06:51 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,15 @@
-CREATE TABLE IF NOT EXISTS public.urls (
+CREATE TABLE IF NOT EXISTS urls (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE
+);
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id INT REFERENCES urls (id),
+    status_code INT,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
     created_at DATE
 );
