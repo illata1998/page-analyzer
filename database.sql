1b6c09e0f031fe6f5a09549b7ff9d8f2fa89dabd
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-06-23 19:00:29 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS urls (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    created_at DATE
+    created_at TIMESTAMP DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
@@ -11,5 +11,18 @@ CREATE TABLE IF NOT EXISTS url_checks (
     h1 TEXT,
     title TEXT,
     description TEXT,
-    created_at DATE
+    created_at TIMESTAMP DEFAULT now()
 );
+
+CREATE OR REPLACE VIEW latest_url_checks AS
+SELECT
+	t1.url_id,
+	t1.status_code,
+	t1.created_at
+FROM url_checks AS t1
+WHERE t1.created_at = (
+	SELECT
+		MAX(t2.created_at)
+	FROM url_checks AS t2
+	WHERE t1.url_id = t2.url_id
+	GROUP BY t2.url_id);
