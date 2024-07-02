1aecef2f8f2b6181930249b3fed21018d162bc47
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-07-02 17:40:32 +0300

    Update html_parser.py

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 
 def parse_html(resp):
     soup = BeautifulSoup(resp.text, 'html.parser')
-    h1 = soup.h1.string if soup.h1 else ''
-    title = soup.title.string if soup.title else ''
-    description = ''
+    parsed_values = {
+        'h1': soup.h1.string if soup.h1 else '',
+        'title': soup.title.string if soup.title else '',
+        'description': ''
+    }
     for meta in soup.find_all('meta'):
         if meta.get('name') == 'description':
-            description = meta.get('content')
+            parsed_values['description'] = meta.get('content')
             break
-    return h1, title, description
+    return parsed_values
