059534a054d65ce50e202e754ccf762c074dc969
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-07-16 11:35:29 +0300

    Update and rename database_functions.py

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -14,50 +14,39 @@ def format_timestamp(created_at):
 
 
 def add_new_url(url_name):
+    query = 'INSERT INTO urls (name) VALUES (%s);'
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            f"""
-            INSERT INTO urls (name) VALUES
-            ('{url_name}');
-            """
-        )
+        cursor.execute(query, (url_name,))
         conn.commit()
 
 
-def add_new_url_check(url_id, status_code, h1, title, description):
+def add_new_url_check(values):
+    query = '''
+    INSERT INTO url_checks
+    (url_id, status_code, h1, title, description)
+    VALUES (
+        %(url_id)s,
+        %(status_code)s,
+        %(h1)s,
+        %(title)s,
+        %(description)s
+    );
+    '''
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            f"""
-            INSERT INTO url_checks (
-                url_id,
-                status_code,
-                h1,
-                title,
-                description
-            ) VALUES (
-                {url_id},
-                {status_code},
-                $${h1}$$,
-                $${title}$$,
-                $${description}$$
-            );
-            """
-        )
+        cursor.execute(query, values)
         conn.commit()
 
 
 def get_url_id_by_url_name(url_name):
+    query = '''
+    SELECT urls.id FROM urls
+    WHERE urls.name = %s;
+    '''
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            f"""
-            SELECT urls.id
-            FROM urls
-            WHERE urls.name = '{url_name}';
-            """
-        )
+        cursor.execute(query, (url_name,))
         record = cursor.fetchone()
     url_id = record[0] if record else None
     return url_id
@@ -65,15 +54,14 @@ def get_url_id_by_url_name(url_name):
 
 def get_url_info_by_url_id(url_id):
     url_info = {}
+    query = '''
+    SELECT *
+    FROM urls
+    WHERE urls.id = %s;
+    '''
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            f"""
-            SELECT *
-            FROM urls
-            WHERE urls.id = {url_id};
-            """
-        )
+        cursor.execute(query, (url_id,))
         record = cursor.fetchone()
     if record:
         url_info['id'] = record[0]
@@ -84,22 +72,21 @@ def get_url_info_by_url_id(url_id):
 
 def get_url_checks_by_url_id(url_id):
     url_checks = []
+    query = '''
+    SELECT
+        url_checks.id,
+        url_checks.status_code,
+        url_checks.h1,
+        url_checks.title,
+        url_checks.description,
+        url_checks.created_at
+    FROM url_checks
+    WHERE url_checks.url_id = %s
+    ORDER BY url_checks.id DESC;
+    '''
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            f"""
-                SELECT
-                    url_checks.id,
-                    url_checks.status_code,
-                    url_checks.h1,
-                    url_checks.title,
-                    url_checks.description,
-                    url_checks.created_at
-                FROM url_checks
-                WHERE url_checks.url_id = {url_id}
-                ORDER BY url_checks.id DESC;
-                """
-        )
+        cursor.execute(query, (url_id,))
         records = cursor.fetchall()
     if records:
         for record in records:
@@ -117,21 +104,20 @@ def get_url_checks_by_url_id(url_id):
 
 def get_all_urls():
     urls = []
+    query = '''
+    SELECT
+        urls.id,
+        urls.name,
+        latest_url_checks.status_code,
+        latest_url_checks.created_at
+    FROM urls
+    LEFT JOIN latest_url_checks
+        ON urls.id = latest_url_checks.url_id
+    ORDER BY urls.id DESC;
+    '''
     with psycopg2.connect(DATABASE_URL) as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            """
-            SELECT
-                urls.id,
-                urls.name,
-                latest_url_checks.status_code,
-                latest_url_checks.created_at
-            FROM urls
-            LEFT JOIN latest_url_checks
-                ON urls.id = latest_url_checks.url_id
-            ORDER BY urls.id DESC;
-            """
-        )
+        cursor.execute(query)
         records = cursor.fetchall()
     if records:
         for record in records:
