440bcbd42bd84bb2710acf8a0918b978938057f7
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-07-16 11:35:39 +0300

    Update and rename html_parser.py

--- page_analyzer/html.py
+++ page_analyzer/html.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
 
-def parse_html(resp):
-    soup = BeautifulSoup(resp.text, 'html.parser')
+def parse_seo_tags_from_html(content):
+    soup = BeautifulSoup(content, 'html.parser')
     parsed_values = {
         'h1': soup.h1.string if soup.h1 else '',
         'title': soup.title.string if soup.title else '',
